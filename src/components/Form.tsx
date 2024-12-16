fc7e44591d4d934f83b1eefd5dc449df0081f80f
kevinocotan <kaocotan@gmail.com>
2024-12-16 10:56:26 -0600

    Validacion de formulario

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -19,6 +19,11 @@ export default function Form() {
         })
     }
 
+    const isValidActivity = () => {
+        const {name, calories} = activity
+        return name.trim() !== '' && calories > 0 
+    }
+
     return (
         <form className="space-y-5 bg-white shadow p-10 rounded-lg">
             <div className='grid grid-cols-1 gap-3'>
@@ -65,8 +70,9 @@ export default function Form() {
 
             <input
                 type="submit"
-                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
-                value='Guardar Comida o Guardar Ejercicio'
+                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
+                value='Guardar Comida o Guardar Ejercicio' 
+                disabled={!isValidActivity()}
             />
 
         </form>
