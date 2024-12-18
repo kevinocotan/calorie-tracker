96387f953c002c32d293256ad5216e13bbfe6df4
kevinocotan <kaocotan@gmail.com>
2024-12-18 11:19:32 -0600

    boton eliminar

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,7 +1,7 @@
 import { useMemo, Dispatch } from "react"
 import { Activity } from "../types"
 import { categories } from "../data/categories"
-import { PencilSquareIcon } from "@heroicons/react/16/solid"
+import { PencilSquareIcon, XCircleIcon } from "@heroicons/react/16/solid"
 import { ActivityActions } from "../reducers/activity-reducer"
 
 type ActivityListProps = {
@@ -46,6 +46,14 @@ export default function ActivityList({ activities, dispatch }: ActivityListProps
                         >
                             <PencilSquareIcon
                                 className="h-8 w-8 text-gray-800"
+                            />
+                        </button>
+
+                        <button
+                            onClick={() => dispatch({ type: "delete-activity", payload: { id: activity.id } })}
+                        >
+                            <XCircleIcon
+                                className="h-8 w-8 text-red-500"
                             />
                         </button>
                     </div>
