9c50df44c57614ff40c6128a30bc5b7af2b018f0
kevinocotan <kaocotan@gmail.com>
2024-12-18 10:08:17 -0600

    recuperando los datos al darle editar

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,13 @@
-import { useState, ChangeEvent, FormEvent, Dispatch } from "react"
+import { useState, ChangeEvent, FormEvent, Dispatch, useEffect } from "react"
 import { v4 as uuidv4 } from "uuid"
 
 import { Activity } from "../types"
 import { categories } from "../data/categories"
-import { ActivityActions } from "../reducers/activity-reducer"
+import { ActivityActions, ActivityState } from "../reducers/activity-reducer"
 
 type FormProps = {
     dispatch: Dispatch<ActivityActions>
+    state: ActivityState
 }
 
 const initialState: Activity = {
@@ -16,9 +17,16 @@ const initialState: Activity = {
     calories: 0
 }
 
-export default function Form({ dispatch }: FormProps) {
+export default function Form({ dispatch, state }: FormProps) {
 
     const [activity, setActivity] = useState<Activity>(initialState)
+
+    useEffect(() => {
+        if (state.activeId) {
+            const selectedActiviy = state.activities.filter(stateActivity => stateActivity.id === state.activeId)[0]
+            setActivity(selectedActiviy)
+        }
+    }, [state.activeId])
 
     const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
         const isNumberField = ['category', 'calories'].includes(e.target.id)
