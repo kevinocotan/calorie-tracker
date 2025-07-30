2727f1c9cf061c00808c1aa3ee700fbad62d87f2
kevinocotan <kaocotan@gmail.com>
2025-07-29 23:40:26 -0600

    Finalizando la migración y Building

--- src/App.tsx
+++ src/App.tsx
@@ -36,8 +36,7 @@ function App() {
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
           <Form
-            dispatch={dispatch}
-            state={state}
+            
           />
         </div>
       </section>
