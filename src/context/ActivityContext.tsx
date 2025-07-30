e54e83ae0e2f7b0c0363dc1601c355b1e260ad4c
kevinocotan <kaocotan@gmail.com>
2025-07-29 23:38:10 -0600

    Importando el Context en las Actividades

--- src/context/ActivityContext.tsx
+++ src/context/ActivityContext.tsx
@@ -1,5 +1,7 @@
 import { Dispatch, ReactNode, createContext, useMemo, useReducer } from 'react';
 import { ActivityActions, activityReducer, ActivityState, initialState } from '../reducers/activity-reducer';
+import { categories } from '../data/categories';
+import { Activity } from '../types';
 
 type ActivityProviderProps = {
     children: ReactNode
@@ -10,7 +12,9 @@ type ActivityContextProps = {
     dispatch: Dispatch<ActivityActions>
     caloriesConsumed: number,
     caloriesBurned: number,
-    netCalories: number
+    netCalories: number,
+    categoryName: (category: Activity['category']) => string[],
+    isEmptyActivities: boolean
 }
 
 export const ActivityContext = createContext<ActivityContextProps>({} as ActivityContextProps);
@@ -30,13 +34,22 @@ export const ActivityProvider = ({ children }: ActivityProviderProps) => {
 
     const netCalories = useMemo(() => caloriesConsumed - caloriesBurned, [state.activities])
 
+    const categoryName = useMemo(() =>
+        (category: Activity['category']) =>
+            categories.map(cat => cat.id === category ? cat.name : '')
+        , [state.activities])
+
+    const isEmptyActivities = useMemo(() => state.activities.length === 0, [state.activities])
+
     return (
         <ActivityContext.Provider value={{
             state,
             dispatch,
             caloriesConsumed,
             caloriesBurned,
-            netCalories
+            netCalories,
+            categoryName,
+            isEmptyActivities
         }}>
             {children}
         </ActivityContext.Provider>
